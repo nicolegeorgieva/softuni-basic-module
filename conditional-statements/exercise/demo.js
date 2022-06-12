5cc9fd6e970fa080e47a0e8dc8c6be36fcf5456e
Nicole <nicole.georgievva@gmail.com>
2022-06-12 20:40:24 +0300

    Add 2 tasks to exercise

---
+++ conditional-statements/exercise/demo.js
@@ -0,0 +1,10 @@
+function demo() {
+    let number = 5;
+    let secondNumber = "5";
+
+    let result = number !== secondNumber;
+
+    console.log(result);
+}
+
+demo();
---
+++ conditional-statements/exercise/ifElse.js
@@ -0,0 +1,13 @@
+function demo() {
+    let number = 11;
+
+    if (number < 10) {
+        console.log("Number is < 10");
+    } else if (number < 15) {
+        console.log("Number is < 15");
+    } else if (number < 20) {
+        console.log("Number is < 20");
+    }
+}
+
+demo();
