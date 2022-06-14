6be2f679c3167b73f66b62b96fb400f0804efee3
Nicole <nicole.georgievva@gmail.com>
2022-06-14 23:44:15 +0300

    Add 3 tasks to lab

---
+++ conditional-statements-advanced/lab/logicalAnd.js
@@ -0,0 +1,9 @@
+function demo() {
+    let number = 6;
+
+    if (number > 5 && number < 10 && number % 2 === 0) {
+        console.log(number);
+    }
+}
+
+demo();
---
+++ conditional-statements-advanced/lab/or.js
@@ -0,0 +1,9 @@
+function demo() {
+    let number = 3000;
+
+    if (number > 5 || number < 10) {
+        console.log(number);
+    }
+}
+
+demo();
