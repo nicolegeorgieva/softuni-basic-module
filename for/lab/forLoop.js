73511c91d43f063008d5151051a488d40416d567
Nicole <nicole.georgievva@gmail.com>
2022-06-23 23:20:54 +0300

    Add 3 tasks to lab

---
+++ for/lab/forLoop.js
@@ -0,0 +1,7 @@
+function solve() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+solve();
---
+++ for/lab/forLoop2.js
@@ -0,0 +1,9 @@
+function solve() {
+    let i = 0;
+
+    for (i++; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+solve();
