69bc4b7f59ab3732d5541633079f988c9df8ea90
Nicole <nicole.georgievva@gmail.com>
2022-07-31 23:45:08 +0300

    Add `accountBalance` task

---
+++ while/lab/balance.js
@@ -0,0 +1,23 @@
+function accountBalance(input) {
+    let index = 0;
+    let account = 0;
+    let command = input[index];
+    index++;
+
+    while (command !== "NoMoreMoney") {
+        let money = Number(command);
+
+        if (money < 0) {
+            console.log("Invalid operation!");
+            break;
+        }
+        account += money;
+        console.log(`Increase: ${money.toFixed(2)}`);
+        command = input[index];
+        index++;
+    }
+
+    console.log("Total: " + account.toFixed(2));
+}
+
+accountBalance(["5.51", "69.42", "100", "NoMoreMoney"]);
