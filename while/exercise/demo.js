940105b59ae13da0236045beda85cc4a515e975e
Nicole <nicole.georgievva@gmail.com>
2022-08-02 23:08:29 +0300

    Add `demo` task

---
+++ while/exercise/demo.js
@@ -0,0 +1,16 @@
+function whileDemo() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+
+    let num = 1;
+
+    while (num <= 5) {
+        console.log(num);
+        num++;
+    }
+
+    console.log(num);
+}
+
+whileDemo();
