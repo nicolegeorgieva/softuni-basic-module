8182ab26b5f396c7f582b14f3eed601303d0e14f
Nicole <nicole.georgievva@gmail.com>
2022-08-18 23:51:33 +0300

    Add `comparison` task

---
+++ exercise/comparison.js
@@ -0,0 +1,11 @@
+let isGreater = (5 + 3) > (3 + 4);
+
+console.log(isGreater);
+
+//__________________________________________
+
+if ("Nicole" == "nicole") {
+    console.log("Nicole");
+} else {
+    console.log("no");
+}
