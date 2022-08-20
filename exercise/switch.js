371e0e5b11f222d296669823f8332cd1cdee16b6
Nicole <nicole.georgievva@gmail.com>
2022-08-20 22:50:00 +0300

    Add `switch` task

---
+++ exercise/switch.js
@@ -0,0 +1,12 @@
+let i = 0;
+
+while (i < 4) {
+    switch (i) {
+        case 1: console.log(i);
+        case 2: console.log(i);
+            break;
+        case 3: console.log(i);
+            break;
+    }
+    i++;
+}
