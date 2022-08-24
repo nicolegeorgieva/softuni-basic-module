8bda3d3c7f7d770a1f2f9b6dc50991dbc1793e39
Nicole <nicole.georgievva@gmail.com>
2022-08-24 19:24:56 +0300

    Add `solve` task

---
+++ exercises/solve.js
@@ -0,0 +1,17 @@
+let a = 9;
+let b = 4;
+let product = a % b;
+
+console.log(product);
+
+//_____________________
+
+let odd = 3 % 2;
+let even = 8 % 2;
+let error = 3 % 0;
+let c = 105 % 10;
+
+console.log(odd);
+console.log(even);
+console.log(error);
+console.log(c);
